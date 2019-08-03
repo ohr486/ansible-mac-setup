2ffdee355ff505c56f8a4f220f7c535644638a04
ohr486 <ohr486@gmail.com>
2019-08-03 20:21:37 +0900

    add alias

---
+++ roles/alias/files/alias.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# for alias by ansible-mac-setup
+
+# for git
+alias g='cd $(ghq root)/$(ghq list | peco)'
+alias gl='open https://$(ghq list | peco | sed -e "s/:10022//g")'
+alias gc='git checkout $(git branch | peco | sed -e "s/\*//g")'
+alias git-private-setup='git config user.name "ohr486" && git config user.email "ohr486@gmail.com"'
+
+# for ruby
+alias be='bundle exec'
+
+# for k8s
+alias k='kubectl'
+alias kg='kubectl get'
+alias kd='kubectl describe'
