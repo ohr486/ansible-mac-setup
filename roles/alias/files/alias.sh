a0f643ad07fd4d634b680a4ee5258463eabcb87e
ohr486 <ohr486@gmail.com>
2023-08-26 23:28:37 +0900

    add notion

--- roles/alias/files/alias.sh
+++ roles/alias/files/alias.sh
@@ -10,6 +10,7 @@ alias g='cd $(ghq root)/$(ghq list | peco)'
 alias gl='open https://$(ghq list | peco | sed -e "s/:10022//g")'
 alias gc='git checkout $(git branch | peco | sed -e "s/\*//g")'
 alias git-private-setup='git config user.name "ohr486" && git config user.email "ohr486@gmail.com"'
+alias git-office-setup='git config user.name "ohara_tsunenori" && git config user.email "tsunenori.ohara@drecom.co.jp"'
 
 # for ruby
 alias be='bundle exec'
